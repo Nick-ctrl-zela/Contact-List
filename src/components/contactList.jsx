6a55b9e06e251502f8eace0d40535312b256db10
LauraKuzara <kuzaralaura@gmail.com>
2024-02-14 21:07:55 -0500

    completed step 5 of first tab

---
+++ src/components/contactList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function contactList() {
+
+    return (
+        <table>
+            <thread>
+                <tr>
+                    <th colSpan="3"> Contact List</th>
+                </tr>
+            </thread>
+            <tbody>
+                <tr>
+                    <td> Name </td>
+                    <td> E-mail </td>
+                    <td> Phone </td>
+                </tr>
+                {
+                    //map data
+                }
+            </tbody>
+        </table>
+    );
+}
