dd7b931f94a7a81821267f471cf0161d68ad7120
LauraKuzara <kuzaralaura@gmail.com>
2024-02-14 21:22:12 -0500

    fixed contactlist error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-
+import ContactList from './components/ContactList';
 import './App.css'
 
 function App() {
+    return (
+        <>
+            <ContactList />
+        </>
+    );
 }
 
 export default App
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 
-export default function contactList() {
+export default function ContactList() {
 
     return (
         <table>
